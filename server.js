f76e7c47a5357789f5d70a4c8e38473eb0c0c626
Lami Beach <oogunde0413@gmail.com>
2020-04-08 16:45:50 -0700

    modified

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 
 require('dotenv').config();
 
-const db = require('db')
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-})
+// const db = require('db')
+// db.connect({
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS
+// })
