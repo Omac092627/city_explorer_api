32d09743bcb304d0168b2a054308accca5e79784
Lami Beach <oogunde0413@gmail.com>
2020-04-08 17:19:53 -0700

    added proper files

--- server.js
+++ server.js
@@ -2,9 +2,27 @@
 
 require('dotenv').config();
 
-// const db = require('db')
-// db.connect({
-//   host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS
-// })
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+app.get('/data', (request, response) => {
+  let airplanes = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well Trained',
+  };
+  response.status(200).json(airplanes);
+});
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+© 2020 GitHub, Inc.
